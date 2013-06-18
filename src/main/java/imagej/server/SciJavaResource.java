a548644aa07b66a3596a3a925038e8bbf11b3ce1
Curtis Rueden <ctrueden@wisc.edu>
2013-06-18 16:31:38 -0500

    WIP: playing around

--- src/main/java/imagej/server/SciJavaResource.java
+++ src/main/java/imagej/server/SciJavaResource.java
@@ -26,22 +26,22 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-/**
- * Root resource (exposed at "myresource" path).
- */
-@Path("myresource")
-public class MyResource {
+import org.scijava.Context;
 
-	/**
-	 * Method handling HTTP GET requests. The returned object will be sent to the
-	 * client as "text/plain" media type.
-	 * 
-	 * @return String that will be returned as a text/plain response.
-	 */
+/** Resource which obtains the SciJava application context. */
+@Path("context")
+public class SciJavaResource {
+
+	private Context context;
+
+	/** Method handling HTTP GET requests. */
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
-	public String getIt() {
-		return "Got it!";
+	@Produces(MediaType.APPLICATION_JSON)
+	public Context context() {
+		if (context == null) {
+			context = new Context();
+		}
+		return context;
 	}
 
 }
